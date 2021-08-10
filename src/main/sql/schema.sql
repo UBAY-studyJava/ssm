db4a8e4d8abc6a39970d7bce388bfeba1fc30cb2
JANG SE YEONG <ainsamel@gmail.com>
2021-08-10 15:28:26 +0900

    change jsp location, change sql to English

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -1,24 +1,24 @@
--- 创建图书表
+-- 创建图书表 도서표 만들기
 CREATE TABLE `book` (
-  `book_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '图书ID',
-  `name` varchar(100) NOT NULL COMMENT '图书名称',
-  `number` int(11) NOT NULL COMMENT '馆藏数量',
+  `book_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'bookID',
+  `name` varchar(100) NOT NULL COMMENT 'name', --도서명
+  `number` int(11) NOT NULL COMMENT 'number', --소장수량
   PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='图书表';
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='bookList'; --도서목록
 
--- 初始化图书数据
+-- 初始化图书数据 도서 데이터 초기화
 INSERT INTO `book` (`book_id`, `name`, `number`)
 VALUES
-	(1000, 'Java程序设计', 10),
-	(1001, '数据结构', 10),
-	(1002, '设计模式', 10),
-	(1003, '编译原理', 10);
+	(1000, 'JavaProgramming', 10), --자바프로그래밍
+	(1001, 'dataStructure', 10), --데이터구조
+	(1002, 'desigModel', 10), --디자인 모델
+	(1003, 'EditPrinciples', 10); --편집 번역 원리
 
--- 创建预约图书表
+-- 创建预约图书表 예약 도서표 만들기
 CREATE TABLE `appointment` (
-  `book_id` bigint(20) NOT NULL COMMENT '图书ID',
-  `student_id` bigint(20) NOT NULL COMMENT '学号',
-  `appoint_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '预约时间' ,
+  `book_id` bigint(20) NOT NULL COMMENT 'bookID',
+  `student_id` bigint(20) NOT NULL COMMENT 'studentId',
+  `appoint_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'ReservationTime' ,
   PRIMARY KEY (`book_id`, `student_id`),
   INDEX `idx_appoint_time` (`appoint_time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='预约图书表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='BookAppointment'; --예약도서표
