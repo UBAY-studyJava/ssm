3a421555036d5a978d258cedf8d82a8798cf6c65
Tim <felzaexilar@gmail.com>
2021-08-16 14:21:49 +0800

    sql scheme create user table added

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -22,3 +22,10 @@ CREATE TABLE `appointment` (
   PRIMARY KEY (`book_id`, `student_id`),
   INDEX `idx_appoint_time` (`appoint_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='BookAppointment';
+
+CREATE TABLE users (
+  user_id VARCHAR(255) NOT NULL COMMENT 'bookID',
+  user_name VARCHAR(255) NOT NULL COMMENT 'studentId',
+  user_pwd VARCHAR(255) NOT NULL COMMENT 'saltedpassword',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='usertable';
